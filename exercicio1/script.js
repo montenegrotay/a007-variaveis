65427d37ee602440093531cb6dcb744478c242f4
Taynara <montenegro.taynara@gmail.com>
2022-08-29 20:40:57 -0300

    Exercícios JS

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+let nome;
+let idade; 
+
+console.log(typeof nome) //string
+console.log(typeof idade) // number
+
+
+let seuNome= prompt("Qual é o seu nome?")
+let suaIdade= prompt("Quantos anos você tem?")
+
+console.log ("Olá", nome, "você tem", idade, "anos.")
+console.log(nome, idade)
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,19 @@
+let a = 10
+let b = 20
+
+let c = a
+a = b
+b = c
+
+
+//Aqui faremos uma lógica para troca de valores
+console.log(a)
+console.log(b)
+
+
+
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) //o valor de a é 20
+console.log("O novo valor de b é", b) //o valor de b é 10
+
